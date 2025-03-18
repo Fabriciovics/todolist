fdcb4632c09e63d7dc587a98033081f38c6b6ffb
fabriciovics <fabriciovics@gmail.com>
2025-03-18 18:46:27 -0300

    first commit

---
+++ src/test/java/vics/projects/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package vics.projects.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
